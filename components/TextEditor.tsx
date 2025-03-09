532ab751ab45626ad65904fc7e1b8ec835030d53
Anish Patil <anishpatil304@gmail.com>
2025-03-09 18:51:14 +0530

    ind user in live status added in FE

--- components/TextEditor.tsx
+++ components/TextEditor.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 
 const TextEditor = () => {
+    const arr:any = [
+        1,3,4,5
+    ]
     return (
         <div className='h-screen flex bg-gray-900 text-white'>
-            <div className='w-[25%] hidden lg:block mx-4 my-5 h-min-[40vh] h-[80vh] h-max-[auto] border-2 border-amber-50'>
-
+            <div className='w-[25%] px-5 py-5 hidden lg:block mx-4 my-5 h-min-[40vh] h-[80vh] h-max-[auto] border-2 border-amber-50'>
+                <div className='w-[45%] h-[150px] flex justify-center items-center bg-gray-600 rounded-[12px]'>
+                    <div className='w-[80%] h-[80%] bg-white flex justify-center items-center text-center rounded-full'>
+                        <p className='text-2xl text-black'>
+                            AN
+                        </p>
+                    </div>
+                </div>
             </div>
             <div className='w-[75%] d-col justify-start items-center mx-4  h-screen '>
                 <div className='h-[80%] w-[90%] mt-5  bg-white border-0 border-amber-100'>
